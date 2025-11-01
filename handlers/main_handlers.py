849b29f4feb49d4c0e3a17d17a8c98872606a0f8
Vuacheslav <soloomonslava1991@gmail.com>
2025-11-01 18:22:42 +0300

    Добавлен тег помощь менеджера и обработка одноимённого чекбокса

--- handlers/main_handlers.py
+++ handlers/main_handlers.py
@@ -396,7 +396,6 @@ async def answer_message(message: Message, bot: Bot):
 @main_router.message(F.web_app_data.data != None)  # Хэндлер для обработки заказа из webapp
 async def web_app_order(message: Message, amo_api: AmoCRMWrapper, fields_id: dict, bot: Bot):
     raw_json = json.loads(message.web_app_data.data)
-    pprint(raw_json)
     full_price = raw_json.get('total')
     contact_id = raw_json.get('userId')
     custom_data = LeadData(raw_json=raw_json, fields_id=fields_id)
